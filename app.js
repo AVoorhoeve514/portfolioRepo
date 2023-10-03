f5c3e0a39b0acd9ef14c37828fc1597266a61ce8
AVoorhoeve514 <voor0056@hz.nl>
2023-10-03 12:05:54 +0200

    random ding toevoegen

---
+++ app.js
@@ -0,0 +1,9 @@
+if (name = joebiden) = true {
+    console.log("I am Joe Biden and I approve this message")
+} else {
+    console.log("Name is not Joe Biden");
+}
+    console.log("I am not Joe Biden");
+}
+    console.log("I am not Joe Biden")
+}
